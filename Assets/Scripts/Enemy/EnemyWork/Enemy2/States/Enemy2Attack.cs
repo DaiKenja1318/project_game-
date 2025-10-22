39735beb2575b8a73efbb9f5e0b41d2ae012edc9
VietHung <hungdam969@gmail.com>
2025-10-22 10:06:56 +0700

    A star Path finding for enemy sight

---
+++ Assets/Scripts/Enemy/EnemyWork/Enemy2/States/Enemy2Attack.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+
+namespace Enemy.EnemyWork.Enemy2.States
+{
+    public class Enemy2Attack : Enemy2State
+    {
+        [SerializeField] private float shootingRange = 7f;
+        [SerializeField] private Transform movebackPoint;
+        public override Enemy2State State(Enemy2Controller controller)
+        {
+            if (Vector2.Distance(controller.transform.position, controller.target.position) > shootingRange)
+            {
+                controller.animator.SetBool("Chase", true);
+                controller.animator.SetBool("Attack", false);
+                controller.CancelInvoke("HandleShooting");
+                return controller.chase;
+            }
+
+            //Attack Codes
+            if (controller.TargetInDistance() && controller.followEnabled)
+            {
+                controller.PathFollow();
+            }
+
+            if (Vector2.Distance(controller.transform.position,controller.target.position) <= 5f)
+            {
+                controller.followEnabled = false;
+                controller.rb.mass = 50f;
+            }
+            else
+            {
+                controller.followEnabled = true;
+                controller.rb.mass = 1f;
+            }
+
+            WeaponAiming(controller);
+
+            return this;
+        }
+
+        private void WeaponAiming(Enemy2Controller controller)
+        {
+            //Weapon Aim
+            Vector2 direction = new Vector2(controller.target.transform.position.x - controller.armPoint.transform.position.x, controller.target.transform.position.y - controller.armPoint.transform.position.y);
+            controller.armPoint.transform.right = direction;
+
+            //Flip NEW
+            Vector3 armPointLocalScale = controller.armPoint.transform.localScale;
+            Vector3 controllerLocalScale = controller.transform.localScale;
+            if (controller.target.transform.position.x <= controller.transform.position.x)
+            {
+                armPointLocalScale.x = -0.8f;
+                armPointLocalScale.y = -0.14f;
+                controllerLocalScale.x = -1f;
+            }
+            if (controller.target.transform.position.x > controller.transform.position.x)
+            {
+                armPointLocalScale.x = +0.8f;
+                armPointLocalScale.y = +0.14f;
+                controllerLocalScale.x = +1f;
+            }
+            controller.armPoint.transform.localScale = armPointLocalScale;
+            controller.transform.localScale = controllerLocalScale;
+        }
+    }
+}
+
+//Flip weapon OLD
+/*float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+Vector3 armPointLocalScale = controller.armPoint.transform.localScale;
+Vector3 controllerLocalScale = controller.transform.localScale;
+if (angle > 90 || angle < -90)
+{
+    armPointLocalScale.x = -0.8f;
+    armPointLocalScale.y = -0.14f;
+    controllerLocalScale.x = -1f;
+}
+else
+{
+
+    armPointLocalScale.x = +0.8f;
+    armPointLocalScale.y = +0.14f;
+    controllerLocalScale.x = +1f;
+}
+controller.armPoint.transform.localScale = armPointLocalScale;
+controller.transform.localScale = controllerLocalScale;*/
--- Assets/Scripts/Lightman.cs
+++ Assets/Scripts/Lightman.cs
@@ -5,71 +5,69 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Lightman: Character
+public class Lightman : Character
 {
+    [Header("Attack Settings")]
     [SerializeField] private Bullet bullet;
+
     void Start()
     {
         base.Start();
     }
 
-    // Update is called once per frame
     void Update()
     {
-       
-       base.Update();
+        base.Update();
     }
 
-    protected override void Skill() // Skill animation
+    protected override void Skill()
     {
-        if (Input.GetKeyDown(KeyCode.I) && isGround && !isAttack && !isDefend) // Kiểm tra nếu nhân vật chưa tấn công
+        if (Input.GetKeyDown(KeyCode.I) && isGround && !isAttack && !isDefend)
         {
-        isAttack = true; // Đánh dấu đang tấn công
-        ChangeAnim("Skill");
-        StartCoroutine(WaitSkill());
-        StartCoroutine(Fire());
+            isAttack = true;
+            ChangeAnim("Skill");
+            StartCoroutine(WaitSkill());
+            StartCoroutine(Fire());
         }
     }
 
-public IEnumerator Fire()
-{
-    yield return new WaitForSeconds(0.28f);
-    float direction = Mathf.Sign(transform.localScale.x);
-    
-    Bullet newbullet = Instantiate(bullet); // Tạo một viên đạn mới
-    newbullet.MoveBullet(transform.position + Vector3.right * direction, direction);
-}
+    public IEnumerator Fire()
+    {
+        yield return new WaitForSeconds(0.28f);
+        float direction = Mathf.Sign(transform.localScale.x);
+        Bullet newBullet = Instantiate(bullet);
+        newBullet.MoveBullet(transform.position + Vector3.right * direction, direction);
+    }
 
-
-    public IEnumerator WaitSkill() //Delay skill animation
+    public IEnumerator WaitSkill()
     {
         yield return new WaitForSeconds(0.7f);
         isAttack = false;
         speed = saveSpeed;
     }
-    
-    protected override void Defend() // Defend animation
+
+    protected override void Defend()
     {
-        if(Input.GetKeyDown(KeyCode.U) && isGround && !isAttack && !isDefend)
+        if (Input.GetKeyDown(KeyCode.U) && isGround && !isAttack && !isDefend)
         {
             ChangeAnim("Defend");
             isDefend = true;
             speed = 0;
-            Invoke("WaitPrepareToTeleport",0.7f);
+            Invoke(nameof(WaitPrepareToTeleport), 0.7f);
         }
     }
 
     public void WaitPrepareToTeleport()
     {
-        float direction = Mathf.Sign(transform.localScale.x); 
-        transform.position = transform.position + new Vector3(3,0,0) * direction;
+        float direction = Mathf.Sign(transform.localScale.x);
+        transform.position += new Vector3(3, 0, 0) * direction;
         StartCoroutine(Teleportationation());
     }
+
     public IEnumerator Teleportationation()
     {
-        yield return new WaitForSeconds(2.04f - 0.4f);
+        yield return new WaitForSeconds(1.64f);
         isDefend = false;
         speed = saveSpeed;
     }
-
 }
