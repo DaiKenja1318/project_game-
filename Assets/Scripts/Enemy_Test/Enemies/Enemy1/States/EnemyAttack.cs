d73f8d57621d2fac24ebf79354e29074265cec27
VietHung <hungdam969@gmail.com>
2025-11-05 01:20:20 +0700

    Enemy State

---
+++ Assets/Scripts/Enemy_Test/Enemies/Enemy1/States/EnemyAttack.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAttack : EnemyState
+{
+    [SerializeField] private float shootingRange = 7f;
+    [SerializeField] private Transform movebackPoint;
+
+    private bool shootingDelayed;
+    public override EnemyState State(EnemyController controller)
+    {
+        if (Vector2.Distance(controller.transform.position, controller.target.position) > shootingRange)
+        {
+            controller.animator.SetBool("Chase", true);
+            controller.animator.SetBool("Attack", false);
+            controller.CancelInvoke("HandleShooting");
+            return controller.chase;
+        }
+
+        //Moving Back
+        if (Vector2.Distance(controller.transform.position, controller.target.position) < 3f)
+        {
+            controller.aiDestinationSetter.target = movebackPoint;
+            controller.aiPath.maxSpeed = controller.speed * 8f;
+        }
+        else
+        {
+            controller.aiDestinationSetter.target = controller.target;
+            controller.aiPath.maxSpeed = controller.speed * 2f;
+        }
+
+        //Flip
+        if (controller.aiPath.desiredVelocity.x >= 0.01f)
+        {
+            controller.transform.localScale = new Vector3(-1.25f, 1.25f, 1.25f);
+        }
+        else if (controller.aiPath.desiredVelocity.x <= -0.01f)
+        {
+            controller.transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
+        }
+
+        //Look at target
+        Vector2 direction = new Vector2(controller.target.transform.position.x - controller.transform.position.x, controller.target.transform.position.y - controller.transform.position.y);
+        controller.transform.up = Vector3.Lerp(controller.transform.up, direction, controller.damping);
+
+        //Attack
+
+        return this;
+    }
+}
---
+++ Assets/Scripts/Enemy_Test/Enemies/Enemy1/States/EnemyChase.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChase : EnemyState
+{
+    [SerializeField] private float attackDistance = 5f;
+
+    public override EnemyState State(EnemyController controller)
+    {
+        if (Vector2.Distance(controller.transform.position, controller.target.position) < attackDistance)
+        {
+            controller.animator.SetBool("Attack", true);
+            controller.InvokeRepeating("HandleShooting", controller.shootingTime, controller.shootingDelay);
+            return controller.attack;
+        }
+
+        //Look at target
+        Vector2 direction = new Vector2(controller.target.transform.position.x - controller.transform.position.x, controller.target.transform.position.y - controller.transform.position.y);
+        controller.transform.up = Vector3.Lerp(controller.transform.up, direction, controller.damping);
+
+        //Chase
+
+        return this;
+    }
+
+}
